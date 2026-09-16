d73365d4624f17518b898ecb25b2ee9393c8dd6c
arkanafaisal <arkana.faisal.r@gmail.com>
2026-09-16 07:30:17 +0000

    tes

---
+++ week1/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int angka;
+    
+    // Output: Menampilkan teks ke layar
+    printf("Masukkan sebuah angka: ");
+    
+    // Input: Membaca angka yang diketik user
+    scanf("%d", &angka);
+    
+    // Output: Menampilkan angka yang baru saja dimasukkan
+    printf("Anda baru saja memasukkan angka: %d\n", angka);
+    
+    return 0;
+}
